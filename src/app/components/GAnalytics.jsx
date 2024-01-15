87018a3993af218f820d6622924dc91d49eb84f9
Hebert <hebertdev82@gmail.com>
2024-01-15 11:17:21 -0300

    Google Analytics

---
+++ src/app/components/GAnalytics.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const GAnalytics = () => (
+  <>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=G-NNMC3R43D8`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-NNMC3R43D8'); `,
+      }}
+    />
+  </>
+);
