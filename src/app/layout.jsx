356cb45f08b0526937150caedabf2672e15b0440
Hebert Richard <hebertdev82@gmail.com>
2024-03-08 09:22:46 -0300

    feat: Google Tag Manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,7 +21,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <meta name="p:domain_verify" content="2c0d52876cc1ebd6ac09b2b35053d4ba" />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P7JLH4K8');</script>
+      <!-- End Google Tag Manager -->`,
+        }}
+      />
+
       <body id="inicio" className="">
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7JLH4K8"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->`,
+          }}
+        />
+
         <BackToTop />
         <NavBar />
         <NavMobile />
