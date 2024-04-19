a04b44590e1f8ae1ba701b72f5edaeab7b4a7b7f
Hebert <hebertdev82@gmail.com>
2024-04-19 18:59:03 -0300

    feat: adding not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="mt-28 align-center text-center align-middle">
+            <h2 className="text-2xl font-bold mb-6">Página não encontrada!</h2>
+            <p>A página que você tentou acessar não pode ser encontrada, pois ela não existe em nosso servidor.</p>
+            <p>Verifique a página acessada e tente novamente.</p>
+        </div>
+    )
+}
